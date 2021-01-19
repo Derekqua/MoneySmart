f7abca8afd36f5cf69c2ab24fce7c2fb2e6bf6a2
Derek Qua <derekqua8@gmail.com>
2021-01-19 10:56:51 +0800

    Initial Commit

---
+++ MoneySmart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneySmart
+//
+//  Created by Derek Qua on 19/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
