ddee54fe4eb1ac02ce8c6f0b7eec9f19888ddae3
Derek Qua <derekqua8@gmail.com>
2021-01-26 21:23:49 +0800

    pod file

---
+++ MoneySmart/CurrencyViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CurrencyViewController.swift
+//  MoneySmart
+//
+//  Created by Derek Qua on 26/1/21.
+//
+
+import Foundation
+import UIKit
+
+class CurrencyViewController:UIViewController{
+    
+    @IBOutlet weak var tblView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    @IBAction func convertBtn(_ sender: Any) {
+        
+        
+    }
+    @IBAction func txtField(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+}
---
+++ Pods/Target Support Files/Pods-MoneySmart/Pods-MoneySmart-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MoneySmart : NSObject
+@end
+@implementation PodsDummy_Pods_MoneySmart
+@end
